bb40e2957fe7b201319235dd6853af0391afe1a8
bobwei <bob61016@gmail.com>
2016-11-11 16:28:52 +0800

    update style, replace page with stateless

--- pages/notes/component-playground/index.jsx
+++ pages/notes/component-playground/index.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React from 'react';
 import DocumentTitle from 'react-document-title';
 import Codemirror from 'react-codemirror';
@@ -6,47 +5,35 @@ import 'codemirror/mode/javascript/javascript';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/solarized.css';
 
-class Page extends React.Component {
-  constructor() {
-    super();
-    this.state = { code: '' };
-    this.updateCode = this.updateCode.bind(this);
-  }
+const Page = () => (
+  <DocumentTitle title={Page.metadata().title}>
+    <div>
+      <h1>React Component Playground</h1>
+      <p>
+        This is a test for showcase of component play ground.
+      </p>
+      <div>
+        <Codemirror
+          options={{
+            lineNumbers: true,
+            mode: 'javascript',
+            theme: 'solarized',
+            tabSize: 2,
+            autofocus: true,
+          }}
+          onChange={(code) => {
+            console.log(code);
+          }}
+        />
+      </div>
+    </div>
+  </DocumentTitle>
+);
 
-  static metadata() {
-    return {
-      title: 'React Component Playground',
-    };
-  }
-
-  updateCode(code) {
-    this.setState({ code });
-  }
-
-  render() {
-    const { code } = this.state;
-    return (
-      <DocumentTitle title={Page.metadata().title}>
-        <div>
-          <h1>React Component Playground</h1>
-          <p>
-            This is a test for showcase of component play ground.
-          </p>
-          <div>
-            <Codemirror
-              value={code}
-              onChange={this.updateCode}
-              options={{
-                lineNumbers: true,
-                mode: 'javascript',
-                theme: 'solarized',
-              }}
-            />
-          </div>
-        </div>
-      </DocumentTitle>
-    );
-  }
-}
+Page.metadata = () => {
+  return {
+    title: 'React Component Playground',
+  };
+};
 
 export default Page;
