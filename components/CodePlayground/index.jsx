c874880b9f0feea7a0f54784326fb7c46cfd7649
bobwei <bob61016@gmail.com>
2016-11-12 18:02:32 +0800

    add codemirror.scss

--- components/CodePlayground/index.jsx
+++ components/CodePlayground/index.jsx
@@ -14,6 +14,7 @@ import withProps from 'recompose/withProps';
 import { execute as run, defaultScope } from 'utils/code';
 import codeTemplate from 'raw!./template';
 import styles from './index.module.scss';
+import './codemirror.scss';
 
 if (canUseDOM) {
   require('codemirror/mode/javascript/javascript');
